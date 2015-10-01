f236a3f25c8cd0a941d71f45fce1d7fa7b1e3dc0
Roy Vanegas <roy@thecodeeducators.com>
2015-10-01 12:56:04 -0400

    Change `plugins` back to `$`

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var uglify = require('gulp-uglify');
@@ -17,17 +17,17 @@ var uglify = require('gulp-uglify');
  */
 gulp.task('styles', function () {
     return gulp.src('app/styles/main.scss')
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.sass({
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
             outputStyle: 'compressed',
             precision: 10,
             includePaths: ['.'],
             onError: console.error.bind(console, '\n\nSass error:\n\n')
         }))
-        .pipe(plugins.postcss([
-            require('autoprefixer-core')({browsers: ['last 1 version']})
+        .pipe($.postcss([
+            require('gulp-autoprefixer')({browsers: ['last 1 version']})
         ]))
-        .pipe(plugins.sourcemaps.write())
+        .pipe($.sourcemaps.write())
         .pipe(gulp.dest('.tmp/styles'))
         .pipe(reload({stream: true}));
 });
