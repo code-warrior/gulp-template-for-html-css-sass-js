212336b73bc1a968f5d1d355ed31d6a0d50572ca
Roy Vanegas <roy@thecodeeducators.com>
2015-10-01 09:19:54 -0400

    Replace `$` with `plugins`

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,24 @@
 'use strict';
 
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var plugins = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var uglify = require('gulp-uglify');
 
 gulp.task('styles', function () {
     return gulp.src('app/styles/main.scss')
-        .pipe($.sourcemaps.init())
-        .pipe($.sass({
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({
             outputStyle: 'compressed',
             precision: 10,
             includePaths: ['.'],
             onError: console.error.bind(console, '\n\nSass error:\n\n')
         }))
-        .pipe($.postcss([
+        .pipe(plugins.postcss([
             require('autoprefixer-core')({browsers: ['last 1 version']})
         ]))
-        .pipe($.sourcemaps.write())
+        .pipe(plugins.sourcemaps.write())
         .pipe(gulp.dest('.tmp/styles'))
         .pipe(reload({stream: true}));
 });
