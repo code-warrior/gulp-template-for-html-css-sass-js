aa0f8ae6543772b3a8ce933eba57a2292b20411c
Roy A Vanegas <roy@thecodeeducators.com>
2016-06-24 20:44:29 -0400

    Add forward slash to value assigned to devSourceFolder

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@
 'use strict';
 
 var gulp             = require('gulp'),
-    devSourceFolder  = 'dev',
+    devSourceFolder  = 'dev/',
     prodTargetFolder = 'prod',
     imagesFolder     = 'img',
     sassCSSFolder    = 'styles';
 
 gulp.task('copyUnprocessedAssetsToProdFolder', function () {
     return gulp.src([
-        devSourceFolder + '/*.*',                           // Source all files,
-        devSourceFolder + '/**',                            // and all folders, but
-        '!' + devSourceFolder + '/' + imagesFolder,         // ignore images;
-        '!' + devSourceFolder + '/**/*.js',                 // ignore JS;
-        '!' + devSourceFolder + '/' + sassCSSFolder + '/**' // ignore Sass/CSS.
+        devSourceFolder + '*.*',                           // Source all files,
+        devSourceFolder + '**',                            // and all folders, but
+        '!' + devSourceFolder + + imagesFolder,         // ignore images;
+        '!' + devSourceFolder + '**/*.js',                 // ignore JS;
+        '!' + devSourceFolder + sassCSSFolder + '/**' // ignore Sass/CSS.
     ], {dot: true}).pipe(gulp.dest(prodTargetFolder));
 });
