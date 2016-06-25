540b9f75cce0bfad8cd87f834642fae1c4f34d7f
Roy A Vanegas <roy@thecodeeducators.com>
2016-06-24 20:46:11 -0400

    Remove extra “+” character

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('copyUnprocessedAssetsToProdFolder', function () {
     return gulp.src([
         devSourceFolder + '*.*',                           // Source all files,
         devSourceFolder + '**',                            // and all folders, but
-        '!' + devSourceFolder + + imagesFolder,         // ignore images;
+        '!' + devSourceFolder + imagesFolder,         // ignore images;
         '!' + devSourceFolder + '**/*.js',                 // ignore JS;
         '!' + devSourceFolder + sassCSSFolder + '/**' // ignore Sass/CSS.
     ], {dot: true}).pipe(gulp.dest(prodTargetFolder));
