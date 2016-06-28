ec4166ec2fa9d0a43bdcd22e990cacb9ee1626ed
Roy A Vanegas <roy@thecodeeducators.com>
2016-06-28 14:44:05 -0400

    Update the default task to show the available tasks

--- gulpfile.js
+++ gulpfile.js
@@ -444,11 +444,26 @@ gulp.task('clean', function () {
 /**
  * DEFAULT
  *
- * This task does nothing. See the output message below.
+ * This task does nothing but list the available tasks in this file.
  */
 gulp.task('default', function () {
     'use strict';
 
-    process.stdout.write('\n\tThis default gulp task does nothing except generate ' +
-            'this message.\n\tRun “gulp --tasks” to see the available tasks.\n\n');
+    var exec = require('child_process').exec;
+
+    exec('gulp --tasks', function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write('An error was likely generated when invoking ' +
+                    'the `exec` program in the default task.');
+        }
+
+        if ('' !== stderr) {
+            process.stdout.write('Content has been written to the stderr stream ' +
+                    'when invoking the `exec` program in the default task.');
+        }
+
+        process.stdout.write('\n\tThis default gulp task does ' + colors.red +
+                'nothing' + colors.black + ' except generate this message.\n\t' +
+                'The available tasks are:\n\n' + stdout);
+    });
 });
