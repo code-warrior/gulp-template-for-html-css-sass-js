d762b42074840b7e53e49fcc5b8712ba34986091
Roy Vanegas <roy@thecodeeducators.com>
2017-04-03 20:14:43 -0400

    Move the “use strict” directive to the proper location

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 /*jslint node: true */
 
-'use strict';
-
 var gulp = require('gulp');
 
 gulp.task('default', function () {
+    'use strict';
+
     process.stdout.write('\n\tThis default gulp task does nothing except generate ' +
         'this message.\n\tRun “gulp --tasks” to see the available tasks.\n\n');
 });
