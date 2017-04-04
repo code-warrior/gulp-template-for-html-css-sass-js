b753b67a90023a05536e8088c354c469755512b0
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-04 08:49:06 -0400

    Remove variables from task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,21 @@
 /*jslint node: true */
 
-'use strict';
-
-var gulp             = require('gulp'),
-    tempCache        = require('gulp-cache'),
-    compressImages   = require('gulp-imagemin'),
-
-    // Folder name variables
-    devSourceFolder  = 'dev',
-    prodTargetFolder = 'prod',
-    imagesFolder     = 'img';
+var gulp = require('gulp');
 
 gulp.task('compressThenCopyImagesToProdFolder', function () {
-    return gulp.src(devSourceFolder + '/' + imagesFolder + '/**/*')
+    'use strict';
+
+    var tempCache = require('gulp-cache'),
+        imageCompressor = require('gulp-imagemin');
+
+    return gulp.src('dev/img/**/*')
         .pipe(tempCache(
-            compressImages({
+            imageCompressor({
                 optimizationLevel: 3, // For PNG files. Accepts 0 – 7; 3 is default.
                 progressive: true,    // For JPG files.
                 multipass: false,     // For SVG files. Set to true for compression.
                 interlaced: false     // For GIF files. Set to true for compression.
             })
         ))
-        .pipe(gulp.dest(prodTargetFolder + '/' + imagesFolder));
+        .pipe(gulp.dest('prod/img'));
 });
