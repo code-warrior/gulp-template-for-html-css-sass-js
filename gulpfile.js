94db20d4b6c17caba15a2e027ed0b9551d1aa952
Roy Vanegas <roy@thecodeeducators.com>
2017-04-03 20:07:31 -0400

    Remove superfluous white space

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 /*jslint node: true */
 
-
-var gulp             = require('gulp'),
-    HTMLValidator    = require('gulp-html'),
-
-    HTMLFiles = ['dev/html/*.html',
-                 'dev/html/**/*.html'];
+var gulp = require('gulp'),
+    HTMLValidator = require('gulp-html'),
+    HTMLFiles = ['dev/html/*.html', 'dev/html/**/*.html'];
 
 gulp.task('validateHTML', function () {
     'use strict';
