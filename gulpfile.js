2792528cf954d19c6e5a531d75c2fc0844938211
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-03 23:35:41 -0400

    Remove all variables from the implementation of the compileJSForDev task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 /*jslint node: true */
 
-var gulp                   = require('gulp'),
-    JSConcatenator         = require('gulp-concat'),
-    devSourceFolder        = 'dev',
-    devTargetFolder        = 'temp',
-    JSFolder               = 'scripts',
-    JSTargetFilename       = 'main.js',
-    preConcatenatedJSFiles = devSourceFolder + '/' + JSFolder + '/*.js',
-    JSDevTargetFolder      = devTargetFolder + '/' + JSFolder;
+var gulp = require('gulp');
 
 gulp.task('compileJSForDev', function () {
     'use strict';
 
-    return gulp.src(preConcatenatedJSFiles)
-        .pipe(JSConcatenator(JSTargetFilename))
-        .pipe(gulp.dest(JSDevTargetFolder));
+    var concatenateJS = require('gulp-concat');
+
+    return gulp.src('dev/scripts/*.js')
+        .pipe(concatenateJS('main.js'))
+        .pipe(gulp.dest('temp/scripts'));
 });
