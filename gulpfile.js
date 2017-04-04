c0b20a86d11a604434d727adcb73b0eff18d8fd4
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-03 23:00:07 -0400

    Replace sassSourceFileForDev with its value

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@
 
 var gulp                           = require('gulp'),
     sass                           = require('gulp-sass'),
-    browserSpecificPrefixGenerator = require('gulp-autoprefixer'),
-    sassSourceFileForDev           = 'dev/styles/00-main-dev/main.scss';
+    browserSpecificPrefixGenerator = require('gulp-autoprefixer');
 
 gulp.task('compileCSSForDev', function () {
     'use strict';
 
-    return gulp.src(sassSourceFileForDev)
+    return gulp.src('dev/styles/00-main-dev/main.scss')
         .pipe(sass({
             outputStyle: 'expanded',
             precision: 10
