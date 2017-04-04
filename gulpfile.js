582bc736dfe5432c87f2e808c2bf989f75ee4df5
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-04 10:50:33 -0400

    Optimize task

--- gulpfile.js
+++ gulpfile.js
@@ -1,135 +1,99 @@
 /*jslint node: true */
 
-'use strict';
-
-    // Gulp plugins
-var gulp                           = require('gulp'),
-    sass                           = require('gulp-sass'),
-    browserSpecificPrefixGenerator = require('gulp-autoprefixer'),
-    HTMLValidator                  = require('gulp-html'),
-    JSConcatenator                 = require('gulp-concat'),
-    JSLinter                       = require('gulp-eslint'),
-    browserSync                    = require('browser-sync'),
-    reload                         = browserSync.reload,
-
-    // Folder name variables
-    devSourceFolder                = 'dev/',
-    devTargetFolder                = 'temp/',
-    HTMLSourceFolder               = 'html',
-    JSFolder                       = 'scripts',
-    imagesFolder                   = 'img',
-    sassCSSFolder                  = 'styles',
-
-    // Filenames
-    JSTargetFilename               = 'main.js',
-
-    preCompiledJSFilesWithGrid    = devSourceFolder + JSFolder + '/*.js',
-    preCompiledJSFilesWithoutGrid = [
-        devSourceFolder + JSFolder + '/*.js',
-        '!' + devSourceFolder + JSFolder + '/grid.js'
-    ],
-
-    HTMLFiles = [
-        devSourceFolder + HTMLSourceFolder + '/*.html',
-        devSourceFolder + HTMLSourceFolder + '/**/*.html'
-    ],
-
-    JSDevTargetFolder        = devTargetFolder  + JSFolder,
-    cssDevDestinationFolder  = devTargetFolder  + sassCSSFolder + '/',
-    sassSourceFileForDev     = devSourceFolder  + sassCSSFolder +
-        '/00-main-dev/main.scss';
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSpecificPrefixer = require('gulp-autoprefixer'),
+    htmlValidator = require('gulp-html'),
+    jsConcatenator = require('gulp-concat'),
+    jsLinter = require('gulp-eslint'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    jsTargetFilename = 'main.js',
+    preCompiledJSFiles = 'dev/scripts/*.js';
 
 gulp.task('validateHTML', function () {
-    return gulp.src(HTMLFiles)
-        .pipe(HTMLValidator());
+    'use strict';
+
+    return gulp.src(['dev/html/*.html', 'dev/html/**/*.html'])
+        .pipe(htmlValidator());
 });
 
 gulp.task('compileCSSForDev', function () {
-    return gulp.src(sassSourceFileForDev)
+    'use strict';
+
+    return gulp.src('dev/styles/00-main-dev/main.scss')
         .pipe(sass({
             outputStyle: 'expanded',
             precision: 10
         }).on('error', sass.logError))
-        .pipe(browserSpecificPrefixGenerator({
+        .pipe(browserSpecificPrefixer({
             browsers: ['last 2 versions']
         }))
-        .pipe(gulp.dest(cssDevDestinationFolder));
+        .pipe(gulp.dest('temp/styles'));
 });
 
 gulp.task('compileJSForDev', function () {
-    return gulp.src(preCompiledJSFilesWithGrid)
-        .pipe(JSConcatenator(JSTargetFilename))
-        .pipe(gulp.dest(JSDevTargetFolder));
+    'use strict';
+
+    return gulp.src(preCompiledJSFiles)
+        .pipe(jsConcatenator(jsTargetFilename))
+        .pipe(gulp.dest('temp/scripts'));
 });
 
 gulp.task('lintJS', function () {
-    return gulp.src(preCompiledJSFilesWithoutGrid)
-        .pipe(JSConcatenator(JSTargetFilename))
-        .pipe(JSLinter({
-            'rules': {
-                'indent': [2, 4],
-                'quotes': [2, 'single'],
+    'use strict';
+
+    return gulp.src(preCompiledJSFiles)
+        .pipe(jsConcatenator(jsTargetFilename))
+        .pipe(jsLinter({
+            rules: {
+                indent: [2, 4],
+                quotes: [2, 'single'],
                 'linebreak-style': [2, 'unix'],
-                'semi': [2, 'always'],
+                semi: [2, 'always'],
                 'max-len': [2, 85, 4]
             },
-            'env': {
-                'node': true,
-                'browser': true
+            env: {
+                node: true,
+                browser: true
             },
-            'extends': 'eslint:recommended'
+            extends: 'eslint:recommended'
         }))
-        .pipe(JSLinter.formatEach('compact', process.stderr))
+        .pipe(jsLinter.formatEach('compact', process.stderr))
         //
         // “To have the process exit with an error code (1) on lint error, return
         // the stream and pipe to failAfterError last.”
         //
         //     — https://github.com/adametry/gulp-eslint
         //
-        .pipe(JSLinter.failAfterError());
+        .pipe(jsLinter.failAfterError());
 });
 
-gulp.task('serve',
-    [
-        'compileCSSForDev',
-        'compileJSForDev',
-        'lintJS',
-        'validateHTML'
-    ],
-    function () {
-        browserSync({
-            notify: true,
-            port: 9000,
-            reloadDelay: 100,
-            server: {
-                baseDir: [
-                    devTargetFolder,
-                    devSourceFolder,
-                    devSourceFolder + HTMLSourceFolder
-                ]
-            }
-        });
+gulp.task('serve', ['compileCSSForDev', 'compileJSForDev', 'lintJS', 'validateHTML'], function () {
+    'use strict';
 
-        gulp.watch(devSourceFolder + JSFolder + '/*.js',
-            ['compileJavaScriptForDev', 'lintJS']).on(
-            'change',
-            reload
-        );
-
-        gulp.watch(devSourceFolder + imagesFolder + '/**/*').on(
-            'change',
-            reload
-        );
-
-        gulp.watch([devSourceFolder + HTMLSourceFolder + '/**/*.html'],
-            ['validateHTML']).on(
-            'change',
-            reload
-        );
-
-        gulp.watch(devSourceFolder + sassCSSFolder + '/**/*.scss',
-            ['compileCSSForDev']).on(
-            'change',
-            reload
-        );
+    browserSync({
+        notify: true,
+        port: 9000,
+        reloadDelay: 100,
+        server: {
+            baseDir: [
+                'temp/',
+                'dev/',
+                'dev/html'
+            ]
+        }
     });
+
+    gulp.watch('dev/scripts/*.js', ['compileJavaScriptForDev', 'lintJS'])
+        .on('change', reload);
+
+    gulp.watch('dev/img/**/*')
+        .on('change', reload);
+
+    gulp.watch(['dev/html/**/*.html'], ['validateHTML'])
+        .on('change', reload);
+
+    gulp.watch('dev/styles/**/*.scss', ['compileCSSForDev'])
+        .on('change', reload);
+});
