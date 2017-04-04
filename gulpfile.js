66580f01ca7272b166606feb7c80ba6e772298ff
Roy Vanegas <roy@thecodeeducators.com>
2017-04-03 20:15:18 -0400

    Indent accordion to JSLint

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,5 @@ gulp.task('default', function () {
     'use strict';
 
     process.stdout.write('\n\tThis default gulp task does nothing except generate ' +
-        'this message.\n\tRun “gulp --tasks” to see the available tasks.\n\n');
+            'this message.\n\tRun “gulp --tasks” to see the available tasks.\n\n');
 });
