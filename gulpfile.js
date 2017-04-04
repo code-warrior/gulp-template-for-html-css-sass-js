3c8ca1a9651ebdafc0cf4de7658ed0ca38690381
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-03 23:04:01 -0400

    Replace browserSpecificPrefixGenerator with browserSpecificPrefixer

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ gulp.task('compileCSSForDev', function () {
     'use strict';
 
     var sass = require('gulp-sass'),
-        browserSpecificPrefixGenerator = require('gulp-autoprefixer');
+        browserSpecificPrefixer = require('gulp-autoprefixer');
 
     return gulp.src('dev/styles/00-main-dev/main.scss')
         .pipe(sass({
             outputStyle: 'expanded',
             precision: 10
         }).on('error', sass.logError))
-        .pipe(browserSpecificPrefixGenerator({
+        .pipe(browserSpecificPrefixer({
             browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest('temp/styles/'));
