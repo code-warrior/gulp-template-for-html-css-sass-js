921511dbdb334be59df2d93dbed4e3b6b7afb46e
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-24 23:36:58 -0400

    Remove grid.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,7 @@ gulp.task('compileJSForProd', function () {
     var jsConcatenator = require('gulp-concat'),
         jsCompressor = require('gulp-uglify');
 
-    return gulp.src([
-        'dev/scripts/*.js',
-        '!dev/scripts/grid.js'
-    ])
+    return gulp.src('dev/scripts/*.js')
         .pipe(jsConcatenator('main.js'))
         .pipe(jsCompressor())
         .pipe(gulp.dest('prod/scripts'));
