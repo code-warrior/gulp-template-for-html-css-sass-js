f1371c68c4ce4f82c524b57fb8871b85d6fa5d6a
Scottk123 <skennedy@hartford.edu>
2021-10-11 15:22:28 -0400

    Update compressHTML function

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,9 @@
 
 const { src, dest }= require('gulp');
 const htmlCompressor = require('gulp-htmlmin');
-    
-gulp.task('compressHTML', function () {
-    'use strict';
 
-    return gulp.src(['dev/html/*.html', 'dev/html/**/*.html'])
-        .pipe(htmlMinifier({
-            removeComments: true,
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest('prod'));
-});
+let compressHTML = () => {
+    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
