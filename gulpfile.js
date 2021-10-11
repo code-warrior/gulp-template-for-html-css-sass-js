50b4862f0e1e225ff7af4023cf70795b7f6c7378
Roy A Vanegas <roy@thecodeeducators.com>
2021-10-11 17:51:41 -0400

    Set the listTasks task as the default task

--- gulpfile.js
+++ gulpfile.js
@@ -20,4 +20,25 @@ async function clean() {
     process.stdout.write(`\n`);
 }
 
+async function listTasks () {
+    let exec = require(`child_process`).exec;
+
+    exec(`gulp --tasks`, function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write(`An error was likely generated when invoking ` +
+                `the “exec” program in the default task.`);
+        }
+
+        if (`` !== stderr) {
+            process.stdout.write(`Content has been written to the stderr stream ` +
+                `when invoking the “exec” program in the default task.`);
+        }
+
+        process.stdout.write(`\n\tThis default task does ` +
+            `nothing but generate this message. The ` +
+            `available tasks are:\n\n${stdout}`);
+    });
+}
+
 exports.clean = clean;
+exports.default = listTasks;
