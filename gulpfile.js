becb6a09baf2c5860b089f5b6858cff1b5754853
Roy Vanegas <roy@thecodeeducators.com>
2023-02-23 08:42:40 -0500

    Add missing space

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ let transpileJSForDev = () => {
 };
 
 let compressHTML = () => {
-    return src([`dev/html/*.html`,`dev/html/**/*.html`])
+    return src([`dev/html/*.html`, `dev/html/**/*.html`])
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
 };
