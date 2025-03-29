87bef4163ee4b81f508024503a92bd17ebece4eb
Roy Vanegas <roy@thecodeeducators.com>
2025-03-29 11:09:24 -0400

    Update the listTasks task

--- gulpfile.js
+++ gulpfile.js
@@ -168,20 +168,14 @@ async function clean() {
 async function listTasks () {
     let exec = require(`child_process`).exec;
 
-    exec(`gulp --tasks`, function (error, stdout, stderr) {
+    exec(`gulp --tasks`, function (error, stdout) {
         if (null !== error) {
-            process.stdout.write(`An error was likely generated when invoking ` +
-                `the “exec” program in the default task.`);
+            console.log(`An error was generated when invoking the “exec”` +
+                `program in the default task.`);
         }
 
-        if (`` !== stderr) {
-            process.stdout.write(`Content has been written to the stderr stream ` +
-                `when invoking the “exec” program in the default task.`);
-        }
-
-        process.stdout.write(`\n\tThis default task does ` +
-            `nothing but generate this message. The ` +
-            `available tasks are:\n\n${stdout}`);
+        console.log(`\n\tThis default task does nothing but generate this ` +
+            `message. The available tasks are:\n\n${stdout}`);
     });
 }
 
